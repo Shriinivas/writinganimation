24e17b9d12fbfb4a36000ab252fccc9311e3be7f
Shrinivas Kulkarni <shrinivk@gmail.com>
2024-06-01 15:45:28 +0530

    Cleanup

--- __init__.py
+++ __init__.py
@@ -1,22 +1,18 @@
 #
 # This Blender add-on creates writing animation for the selected Bezier curves
-# Supported Blender Version: 2.8x
+# Supported Blender Version: 2.8x and above
 #
-# Copyright (C) 2018  Shrinivas Kulkarni
-#
-# License: GPL (https://github.com/Shriinivas/writinganimation/blob/master/LICENSE)
-#
+# Author: Shrinivas Kulkarni (khemadeva@gmail.com)
 
-from . writinganim_2_8 import register, unregister
+from .writinganim import register, unregister
 
 bl_info = {
     "name": "Create Writing Animation",
     "author": "Shrinivas Kulkarni",
     "location": "Properties > Active Tool and Workspace Settings > Create Writing Animation",
     "category": "Animation",
-    "blender": (2, 80, 0),    
+    "blender": (2, 80, 0),
 }
 
 if __name__ == "__main__":
     register()
-
